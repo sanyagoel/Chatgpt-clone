1174ae3acc2cd4e36ac940430065e92032d5eaff
sanyagoel <goelsanya575@gmail.com>
2024-07-17 17:38:55 +0530

    used webpack-for using env variables in static files, started with frontend

--- public/admin.js
+++ public/admin.js
@@ -1,28 +1,38 @@
 //require('dotenv').config();
 
 async function fetchRes(){
-    const message = document.getElementById('question').value;
-    const question = document.getElementById('userques');
-    const answer = document.getElementById('answer');
+    let message = document.getElementById('question').value;
+    document.getElementById('question').value = '';
+    let container = document.getElementById('container');
+   
     console.log(message);
-    question.innerHTML += `<div>${message}</div>`
-    const url = `https://chat-gpt-3-5-turbo2.p.rapidapi.com/?question=${message}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': process.env.RAPID_KEY  ,
-        'x-rapidapi-host': process.env.RAPID_HOST
-      }
-    };
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        answer.innerHTML += `<div>${result.answer} </div>\n`
-        console.log(result.answer);
-    } catch (error) {
-        console.error(error);
-    }
+    container.innerHTML += `<li class="userques">${message}</li>`
+    const url = 'https://infinite-gpt.p.rapidapi.com/infinite-gpt';
+const options = {
+  method: 'POST',
+  headers: {
+    'x-rapidapi-key': process.env.RAPID_KEY,
+    'x-rapidapi-host': process.env.RAPID_HOST,
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    query: `${message}`,
+    sysMsg: 'You are a friendly Chatbot.'
+  })
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+
+    container.innerHTML += `<li class="answer">${result.msg} </li>`
+    container.scrollTo(0, container.scrollHeight);
+
+} catch (error) {
+	console.error(error);
+}    
+
 }
 
 window.fetchRes = fetchRes;
