6364c54cd22060fdbd7a298fcf4696a7d399e122
sanyagoel <goelsanya575@gmail.com>
2024-07-18 14:03:15 +0530

    added copytoclipboard, texttospeech, automaticcardquestions,completedui

--- public/admin.js
+++ public/admin.js
@@ -1,17 +1,60 @@
 // require('dotenv').config();
 
 
-async function fetchRes(){
-    let message = document.getElementById('question').value;
+async function addquestion(button){
+  let message = button.querySelector('p').textContent;
+  fetchRes(message);
+}
+
+
+async function copyClipboard(button){
+  let answer = button.closest('.answer').querySelector('p').textContent;
+  navigator.clipboard.writeText(answer);
+}
+
+async function toSpeech(button){
+  const speechSynth = window.speechSynthesis;
+  let answer = button.closest('.answer').querySelector('p').textContent;
+  console.log(answer);
+
+  if(!speechSynth.speaking && answer.trim().length){
+    const newUtter = new SpeechSynthesisUtterance(answer);
+    speechSynth.speak(newUtter); 
+
+    
+  button.addEventListener('click',matres);
+
+  function matres(){
+      speechSynth.cancel();
+      button.removeEventListener('click',matres); // Remove the event listener after canceling speech
+    
+  }
+
+  }
+
+}
+
+
+async function fetchRes(message){
+  // if(message===undefined){
+  //   message = document.getElementById('question').value;
+  // }
+  let homeOptions = document.getElementById('homeOptions');
+    homeOptions.innerHTML = '';
+  let remove = document.getElementById('removed');
+  if(remove){
+    remove.remove();
+
+  }
     document.getElementById('question').value = '';
     let container = document.getElementById('container');
-   
     console.log(message);
     container.innerHTML += `<li class="userques">${message}</li>`
-    container.innerHtML += `<li>                </li>`
+    container.innerHTML += `<li>                </li>`
 
 
-    const url = 'https://infinite-gpt.p.rapidapi.com/infinite-gpt';
+    const url = 'https://cheapest-gpt-4-turbo-gpt-4-vision-chatgpt-openai-ai-api.p.rapidapi.com/v1/chat/completions';
+
 const options = {
   method: 'POST',
   headers: {
@@ -20,17 +63,34 @@ const options = {
     'Content-Type': 'application/json'
   },
   body: JSON.stringify({
-    query: `${message}`,
-    sysMsg: 'You are a friendly Chatbot.'
-  })
+    messages: [
+      {
+        role: 'user',
+        content: `${message}`
+      }
+    ],
+    model: 'gpt-4-turbo-2024-04-09',
+    max_tokens: 100,
+    temperature: 0.9
+  }
+)
+
 };
 
 try {
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result);
+    console.log(result.choices[0].message.content);
 
-    container.innerHTML += `<li class="answer">${result.msg} </li>`
+    container.innerHTML += `<li class="answer">
+    <div class="logocontainer">
+        <img class="logochatgpt" src="../images/logo.png"/>
+        <p>${result.choices[0].message.content}</p>
+    </div>
+    <button class="answerbuttons" onClick=copyClipboard(this)><img src="../images/logocopy2.png"/></button>
+    <button class="answerbuttons2" onClick=toSpeech(this)><img src="../images/file.png"/></button>
+</li>
+`
     container.scrollTo(0, container.scrollHeight);
 
 } catch (error) {
@@ -40,4 +100,6 @@ try {
 }
 
 window.fetchRes = fetchRes;
-
+window.copyClipboard = copyClipboard;
+window.toSpeech = toSpeech;
+window.addquestion = addquestion;
